fee84dde246b9582ea4788f491914802d38b53d8
王丹 <an.wang@zhan.onaliyun.com>
2021-04-08 17:36:08 +0800

    创建一个新的Swift 项目

---
+++ RbenvAndBundlerTestProject/RbenvAndBundlerTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RbenvAndBundlerTestProject
+//
+//  Created by xzmac on 2021/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red;
+    }
+
+
+}
+
